24a23c98838d048be64bcde92f8e0523cb43b9e7
raju-ahammad <web.rajuahammad@gmail.com>
2021-06-26 23:39:14 +0600

    new commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Particle from "react-particles-js";
 import "../assets/about.css";
 
 import particlesConfig from "../assets/particlesConfig.json";
 import data from "../assets/data.json";
 function App() {
-  const [newData, setNewData] = useState(data)
-  //const newdata = data.map((info)=>console.log(info))
-  console.log(newData);
+ 
+  
+  
   return (
     <>
       <Particle params={particlesConfig} className="App-particles__container" />
@@ -15,9 +15,8 @@ function App() {
         <div className="App-text">
           <h1 className="App-text__title">Animation Test</h1>
           <div className="card__wrapper">
-            {newData.map((data) => (
+            {data.map((data) => (
               <div className="card">
-                
                 <img  src={data.image} alt="Logo" />
                 <p>{data.description}</p>
               </div>
